5c2b9abb406f39a516e3adcf4d714d9345786291
coder-akash <akashd@brightcomgroup.com>
2024-10-09 16:53:48 +0530

    cors policy

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ import reportFormRoute  from './routes/reportform.js'
 
 const app = express()
 
-app.use(cors())
+app.use(cors({credentials:true}))
 app.use(express.json({limit:'50mb'}))
 app.use(cookieParser())
 app.use(express.static('public'));
