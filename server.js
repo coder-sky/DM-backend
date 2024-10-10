3334c6b5469aa666a31dbc703db0740bcd212b92
coder-akash <akashd@brightcomgroup.com>
2024-10-10 10:42:53 +0530

    cors policy

--- server.js
+++ server.js
@@ -15,7 +15,9 @@ import reportFormRoute  from './routes/reportform.js'
 const app = express()
 
 app.use(cors({
- 
+    origin: process.env.FRONTEND_URL,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 }))
 app.use(express.json({limit:'50mb'}))
