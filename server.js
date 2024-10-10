6e2fc03434f0777599fa9eda34ee8a632c62a289
coder-akash <akashd@brightcomgroup.com>
2024-10-10 12:07:23 +0530

    cors policy

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ import reportFormRoute  from './routes/reportform.js'
 const app = express()
 
 app.use(cors({
-  
+    origin: process.env.FRONTEND_URL,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
