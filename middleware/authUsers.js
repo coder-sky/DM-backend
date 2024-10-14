8b85d4bd5329b81a4245ec592ac1c17cc81eda40
coder-akash <akashd@brightcomgroup.com>
2024-10-14 11:42:26 +0530

    cors policy

--- middleware/authUsers.js
+++ middleware/authUsers.js
@@ -12,8 +12,6 @@ export const checkAuthentication = function (req, res, next) {
             role: 'none',
             email: 'none'
         }
-        res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         next()
     }
     else {
@@ -23,8 +21,6 @@ export const checkAuthentication = function (req, res, next) {
                 isAuth: true,
                 ...verifyKey
             }
-            res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
             next()
 
         }
@@ -37,8 +33,6 @@ export const checkAuthentication = function (req, res, next) {
                 role: 'none',
                 email: 'none'
             }
-            res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
             next()
 
         }
